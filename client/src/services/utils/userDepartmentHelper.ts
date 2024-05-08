63d662a73ed2211b22272ff049cb9a61f0e8f15a
dayvid-s <dayvid7santos2002@outlook.com>
2024-05-08 16:05:23 -0300

    feat: added axios and utils

---
+++ client/src/services/utils/userDepartmentHelper.ts
@@ -0,0 +1,5 @@
+export const userDepartments = ['RH', 'Geral - Analista de custos'
+    , 'Farmácia', 'Almoxarifado',
+    'Contabilidade']
+
+export const userRoles = ['Básico', 'Admin ']
---
+++ client/src/services/utils/userHelper.ts
@@ -0,0 +1,34 @@
+import { UserType } from "../@types/user";
+
+export const usersMockup: UserType[] = [
+    {
+        id: 1,
+        name: 'Leia',
+        areaOfManagement: 'RH',
+        password: "123",
+    },
+    {
+        id: 2,
+        name: 'Haroldo',
+        areaOfManagement: 'Geral - Analista de custos',
+        password: "123",
+    },
+    {
+        id: 3,
+        name: 'Fabricio',
+        areaOfManagement: 'Almoxarifado',
+        password: "123",
+    },
+    {
+        id: 4,
+        name: 'Joao',
+        areaOfManagement: 'Almoxarifado',
+        password: "123",
+    },
+    {
+        id: 5,
+        name: 'Rafael',
+        areaOfManagement: 'Analise de custo',
+        password: "123",
+    }
+];
