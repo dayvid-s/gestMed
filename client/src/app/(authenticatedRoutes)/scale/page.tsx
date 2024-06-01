7119805d74963a155df7c6dea01661a62ff43fa8
“Dayvid” <dayvid7santos2002@outlook.com>
2024-05-31 21:07:39 -0300

    feat: created ChoiceScale page

--- client/src/app/(authenticatedRoutes)/scale/page.tsx
+++ client/src/app/(authenticatedRoutes)/scale/page.tsx
@@ -1,12 +1,56 @@
 'use client'
-import React, { useState } from 'react';
+import { ChoiceScale } from '@/components/Choicescale'
+import  { useState } from 'react';
+
+import React, { Dispatch, SetStateAction } from 'react';
+import { Form } from 'rsuite';
+import { SelectPicker } from 'rsuite';
+type UserData = {
+  name: string;
+  email: string;
+  crm: string;
+  uf: string;
+  city: string;
+  phone: string;
+  cpf: string;
+  rg: string;
+  address: string;
+  bank: string;
+  agency: string;
+  account: string;
+  password: string;
+  department: string | null;
+};
+  // role: string | null;
+
+type UserFormProps = {
+  handleInputChange: Dispatch<SetStateAction<UserData>>;
+  userData: UserData;
+  handleDepartmentChange: (value: string | null) => void;
+  handleRoleChange: (value: string | null) => void;
+};
+
+export function UserForm({ handleInputChange, userData,
+  handleDepartmentChange,
+  handleRoleChange
+
+}: UserFormProps) {
+
+  const handleInput = (value: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = event.target;
+    handleInputChange(prevState => ({ ...prevState, [name]: value }));
+  };
+}
 
 export default function Scale() {
+  
+  
 
   return (
     <div className='m-auto mb-20 max-w-screen-2xl'  >
-      <h4 className='mb-5 text-3xl font-semibold ' >Modelos de escala</h4>
+      <h4 className='mb-5 text-3xl font-semibold ' >Modelos de Escala</h4>
 
+  <ChoiceScale/>
     </div>
   );
 };
---
+++ client/src/components/ChoiceScale.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { useState } from "react";
+import { SelectPicker, Stack } from "rsuite";
+import { CreateScaleModal } from "./CreateScaleModal";
+
+export function ChoiceScale() {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+
+    const data = ['Escala padrão',].map(
+        item => ({ label: item, value: item })
+    );
+    return (
+
+        <div>
+
+            <div className="flex-row " >
+                <SelectPicker
+                    className="mr-10"
+                    data={data}
+                    searchable={false}
+                    style={{ width: 224 }}
+                    placeholder="Escolha uma escala"
+                />
+
+                <SelectPicker
+                    data={data}
+                    searchable={false}
+                    style={{ width: 224 }}
+                    placeholder="Escolha uma escala"
+                />
+
+                <button className='border-2 rounded-lg w-44 h-10 bg-[#025959] hover:bg-[#078b8b] text-white m-3' type='submit'>Baixar Escala</button>
+
+
+                <button className='border-2 rounded-lg w-44 h-10 bg-[#025959] hover:bg-[#078b8b] text-white m-3' type='submit'>Limpar Modelo</button>
+
+
+
+                <button
+                    onClick={() => { setModalIsOpen(true) }}
+
+                    className='border-2 rounded-lg w-44 h-10 bg-[#025959] hover:bg-[#078b8b] text-white m-3' type='submit'>Criar Escala</button>
+
+                <CreateScaleModal
+
+                    setIsOpen={setModalIsOpen} modalIsOpen={modalIsOpen} />
+
+            </div>
+
+
+        </div>
+    )
+}
