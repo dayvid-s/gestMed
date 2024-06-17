94c4dc3f541fb546a2eeb59790e3367c1f3f8527
“Dayvid” <dayvid7santos2002@outlook.com>
2024-06-17 00:05:41 -0300

    feat: added more styles at modal

--- client/src/components/ModalToAddUsersToScale.tsx
+++ client/src/components/ModalToAddUsersToScale.tsx
@@ -1,8 +1,8 @@
-import { Button, Modal } from 'rsuite';
+import { Button, Form, Modal, SelectPicker } from 'rsuite';
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { axiosClient } from '../services/axiosClient';
-import { CreateScaleForm } from './CreateScaleForm';
-import { ScaleData, QuantityOfDaysTypes } from '@/@types/scaleTypes';
+import Dayvid from '../assets/dayvid 1.png'
+import Image from 'next/image';
 
 export interface ImodalProps {
     modalIsOpen: boolean;
@@ -11,66 +11,208 @@ export interface ImodalProps {
 
 export function AddUsersToScaleModal({ modalIsOpen, setIsOpen }: ImodalProps) {
     const handleClose = () => setIsOpen(false);
-    const [scaleData, setScaleData] = useState<ScaleData>({
-        name: '',
-        isAutoFilled: false,
-        quantityOfDays: 15
-    });
-    
 
-    const handleIsAutoFilledChange = () => {
-        setScaleData(prevData => ({
-            ...prevData,
-            isAutoFilled: !scaleData.isAutoFilled,
+    const quantityOfDaysOptions = [
+        { label: 'SD', value: 'SD' },
+        { label: 'SN', value: 'SN' },
+    ];
+
+    const [doctorInfo, setDoctorInfo] = useState({
+        name: '',
+        especiality: "",
+        quantityOfDays: 'null', // Valor inicial atualizado para 'SD'
+    });
+
+    const handleInputChange = (name: string, value: string) => {
+        setDoctorInfo(prevState => ({
+            ...prevState,
+            [name]: value
         }));
     };
 
-    const handleWithChangeOfScaleType = (value: QuantityOfDaysTypes | null) => {
-        if (value !== null) {
-            setScaleData(prevData => ({
-                ...prevData,
-                quantityOfDays: value,
-            }));
-        }
-    };
-
-    async function handleSubmit() {
-        try {
-            const response = await axiosClient.post('/user', scaleData);
-
-            if (response.status < 300) {
-                console.log('Usuário criado com sucesso');
-            } else {
-                console.error('Falha ao criar usuário', response);
-            }
-        } catch (error) {
-            console.error('Falha ao criar usuário', error);
-        }
-
-        handleClose();
-    }
 
     return (
         <>
             <Modal className='z-10' backdrop={true} size="lg" open={modalIsOpen} onClose={handleClose}>
                 <Modal.Header>
-                    <h4 className='text-4xl'>Adicionar Médicos</h4>
+                    <h4 className='text-4xl'>Adicionar Médicos na Escala</h4>
                 </Modal.Header>
                 <Modal.Body style={{ height: "80vh" }}>
-                    <CreateScaleForm
-                        handleIsAutoFilledChange={handleIsAutoFilledChange}
-                        handleScaleType={handleWithChangeOfScaleType}
-                        handleInputChange={setScaleData}
-                        scaleData={scaleData}
-                    />
+
+
+                    <Form>
+                        <div className='flex flex-col sm:flex-row flex-wrap gap-y-3.5 justify-between items-center'>
+                            <Form.Group controlId="name">
+                                <Form.ControlLabel>Nome do Médico</Form.ControlLabel>
+                                <Form.Control name="name" value={doctorInfo.name} />
+                            </Form.Group>
+
+
+                            <Form.Group>
+                                <Form.ControlLabel>Especialidade</Form.ControlLabel>
+
+                                <Form.Control name="speciality" value={doctorInfo.especiality} />
+                            </Form.Group>
+
+
+
+                            <Form.Group>
+
+                                <Form.ControlLabel>Turno</Form.ControlLabel>
+                                <SelectPicker
+                                    searchable={false}
+                                    value={doctorInfo.quantityOfDays}
+                                    name="quantityOfDays"
+                                    // onChange={handleScaleType}
+                                    data={quantityOfDaysOptions}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                {/* <Checkbox
+                                    checked={doctorInfo.isAutoFilled}
+                                    onChange={handleIsAutoFilledChange}>
+                                    Preencher Automaticamente
+                                </Checkbox> */}
+                            </Form.Group>
+                        </div>
+                    </Form>
+
+
+
+                    
+                    <div title="Editar conta" className='flex flex-row flex-wrap items-center justify-start w-full gap-x-6 gap-y-4 ' >
+                        
+                        
+                        <div className='p-5 border-2 border-bg[#green500] rounded-md flex flex-row  bg-green-900' >
+                        
+                        <Image quality={100} className="mr-2 cursor-pointer h-18 w-18 " src={Dayvid} alt="Foto de perfil" />
+
+                        {/* <MdAccountCircle className="ml-auto text-black w-7 h-7 " /> */}
+
+                        <div className='flex flex-col cursor-pointer ' >
+
+                            <p className='text-xl font-extrabold text-white '>
+                                Dr. Dayvid Santos
+                                {/* {userName} */}
+                            </p>
+
+                                <span className='text-gray-400 ' > Cardiologista</span>
+                                <span className='text-slate-500 ' > 07:00 - 19:00</span>
+
+                        </div>
+
+                            
+                            
+                            
+                        </div>
+
+                        <div className='p-5 border-2 border-bg[#green500] rounded-md flex flex-row' >
+
+                            <Image quality={100} className="mr-2 cursor-pointer h-18 w-18 " src={Dayvid} alt="Foto de perfil" />
+
+                            {/* <MdAccountCircle className="ml-auto text-black w-7 h-7 " /> */}
+
+                            <div className='flex flex-col cursor-pointer ' >
+
+                                <p className='text-xl font-extrabold text-black '>
+                                    Dr. Dayvid Santos
+                                    {/* {userName} */}
+                                </p>
+
+                                <span className='text-slate-500 ' > Cardiologista</span>
+                                <span className='text-slate-500 ' > 07:00 - 19:00</span>
+
+                            </div>
+
+
+
+
+                        </div>
+                        <div className='p-5 border-2 border-bg[#green500] rounded-md flex flex-row' >
+
+                            <Image quality={100} className="mr-2 cursor-pointer h-18 w-18 " src={Dayvid} alt="Foto de perfil" />
+
+                            {/* <MdAccountCircle className="ml-auto text-black w-7 h-7 " /> */}
+
+                            <div className='flex flex-col cursor-pointer ' >
+
+                                <p className='text-xl font-extrabold text-black '>
+                                    Dr. Dayvid Santos
+                                    {/* {userName} */}
+                                </p>
+
+                                <span className='text-slate-500 ' > Cardiologista</span>
+                                <span className='text-slate-500 ' > 07:00 - 19:00</span>
+
+                            </div>
+
+
+
+
+                        </div>
+                        <div className='p-5 border-2 border-bg[#green500] rounded-md flex flex-row' >
+
+                            <Image quality={100} className="mr-2 cursor-pointer h-18 w-18 " src={Dayvid} alt="Foto de perfil" />
+
+                            {/* <MdAccountCircle className="ml-auto text-black w-7 h-7 " /> */}
+
+                            <div className='flex flex-col cursor-pointer ' >
+
+                                <p className='text-xl font-extrabold text-black '>
+                                    Dr. Dayvid Santos
+                                    {/* {userName} */}
+                                </p>
+
+                                <span className='text-slate-500 ' > Cardiologista</span>
+                                <span className='text-slate-500 ' > 07:00 - 19:00</span>
+
+                            </div>
+
+
+
+
+                        </div>
+                        <div className='p-5 border-2 border-bg[#green500] rounded-md flex flex-row' >
+
+                            <Image quality={100} className="mr-2 cursor-pointer h-18 w-18 " src={Dayvid} alt="Foto de perfil" />
+
+                            {/* <MdAccountCircle className="ml-auto text-black w-7 h-7 " /> */}
+
+                            <div className='flex flex-col cursor-pointer ' >
+
+                                <p className='text-xl font-extrabold text-black '>
+                                    Dr. Dayvid Santos
+                                    {/* {userName} */}
+                                </p>
+
+                                <span className='text-slate-500 ' > Cardiologista</span>
+                                <span className='text-slate-500 ' > 07:00 - 19:00</span>
+
+                            </div>
+
+
+
+
+                        </div>
+                    </div>
+                    
+                    
+                    
+
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={handleClose} appearance="ghost">
-                        Cancelar
-                    </Button>
-                    <Button className='bg-sky-600' onClick={handleSubmit} appearance="primary">
-                        Salvar
-                    </Button>
+                    <button
+                        className='ml-0 md:ml-auto mr-10 min-w-40 border-2 rounded-lg p-3 w-auto h-12 bg-green500 hover:bg-[#39cb76] text-white'
+                        type="button"
+                    >
+                        Redefinir
+                    </button>
+                    <button
+                        className='ml-0 md:ml-auto mr-10 min-w-40 border-2 rounded-lg p-3 w-auto h-12 bg-green500 hover:bg-[#39cb76] text-white'
+                        type="button"
+                    >
+                        Adicionar Médicos
+                    </button>
                 </Modal.Footer>
             </Modal>
         </>
